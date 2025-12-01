5fe1c1aa30bc6a7a25ac1b5114fea28ad550d45f
Santiago <santiagodeandrade4@gmail.com>
2025-11-30 20:35:33 -0400

    Creacion de ConvBlock y modificacion de dep-list

---
+++ utils/ConvBlock.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+class ConvBlock(nn.Module):
+    """A block of Conv2D -> BatchNorm -> ReLU."""
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
+        super(ConvBlock, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        return self.relu(self.bn(self.conv(x)))
