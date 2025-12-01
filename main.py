320bdac4b4108fed2ba346c55e44c904879d5715
santiago <santiagodeandrade4@gmail.com>
2025-12-01 09:52:50 -0400

    Modificaciones para portabilidad

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import enum
-from matplotlib.pyplot import plot
 from pycocotools.coco import COCO
+import torch
 from utils.utils import load_images_paths
-from utils.MACROS import COCO_CLASSES_ES, TRAIN_ANN_FILE, VAL_ANN_FILE
-from utils.utils import get_dataset_classes_count
-from utils.utils import plot_class_distribution
+from utils.MACROS import TRAIN_ANN_FILE, VAL_ANN_FILE
 from utils.YOLO import YOLO
 from utils.YOLODataset import YOLODataset
 from torch import optim
@@ -12,6 +10,7 @@ from utils.yolo_loss import yolo_loss
 from torch.utils.data import DataLoader
 
 
+DEVICE  = "cuda" if torch.cuda.is_available() else "cpu"
 
 if __name__ == "__main__":
 
@@ -19,8 +18,8 @@ if __name__ == "__main__":
     X_train_paths = load_images_paths("./dataset/train2017/train2017/")
     train_dataset = YOLODataset(X_train_paths, Y_train_wrapper)
 
-    Y_val_wrapper = COCO(VAL_ANN_FILE)
-    X_val_paths = load_images_paths("./dataset/val2017/val2017/")
+    #Y_val_wrapper = COCO(VAL_ANN_FILE)
+    #X_val_paths = load_images_paths("./dataset/val2017/val2017/")
 
     TRAIN_LOADER = DataLoader(
             dataset=train_dataset, 
@@ -30,19 +29,18 @@ if __name__ == "__main__":
             pin_memory=True,
             persistent_workers=True)
 
-    model = YOLO(grid_size=7, num_classes=90, num_anchors=3).to("cuda")
+    model = YOLO(grid_size=7, num_classes=90, num_anchors=3).to(DEVICE)
     optimizer = optim.Adam(model.parameters(), lr=0.001)
     criterion = yolo_loss  # Your loss function from earlier
 
-    # Training loop
     num_epochs = 20
     for epoch in range(num_epochs):
         model.train()
         epoch_loss = 0
 
         for images, targets in TRAIN_LOADER:
-            images = images.to('cuda')  # Move to GPU if available
-            targets = targets.to('cuda')
+            images = images.to(DEVICE)  # Move to GPU if available
+            targets = targets.to(DEVICE)
 
             # Forward pass
             predictions = model(images)
